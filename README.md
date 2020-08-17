4efe527971ec334afa711e854f757a8c9f3d8e60
John Doe <pirungu9@gmail.com>
2020-08-17 12:42:50 +0300

    post an envelope

--- README.md
+++ README.md
@@ -3,5 +3,8 @@
 Proof of concept - Docusign Backend API (NodeJS, Express, Axios)
 
 # Debug mode
+To setup the app, copy env.example file into a .env file, populate the settings with the right values from your Docusign account
+
+# Debug mode
 DEBUG=docusign-api:* npm start
 
--- routes/index.js
+++ routes/index.js
@@ -8,16 +8,18 @@ if (process.env.NODE_ENV !== 'production') {
 
 axios.defaults.headers.common['Authorization'] = process.env.BEARER_TOKEN;
 axios.defaults.baseURL = process.env.BASE_URL;
+axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 const accountId = process.env.ACCOUNT;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'the Tasks API' });
+  res.render('index', { title: 'the Docusign APIs' });
 });
 
+/* GET - Get a list of account's Envelopes */
 router.get('/templates', function (req, res, next) {
-  axios.get(`https://demo.docusign.net/restapi/v2/accounts/${accountId}/templates`)
+  axios.get(`accounts/${accountId}/templates`)
   .then(response => {
     console.log(response.data);
     res.status(response.status).send({
@@ -30,5 +32,23 @@ router.get('/templates', function (req, res, next) {
       data: error.response.data
     });
   });
-})
+});
+
+/* POST - Create a new Envelope */
+router.post('/envelopes',  function (req, res, next) {
+  axios.post(`accounts/${accountId}/envelopes`, req.body)
+  .then(response => {
+    console.log(response.data);
+    res.status(response.status).send({
+      data: response.data
+    })
+  })
+  .catch(error => {
+    console.log(error.response);
+    res.status(error.response.status).send({
+      data: error.response.data
+    });
+  }); 
+});
+
 module.exports = router;
