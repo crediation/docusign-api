0c45afe090f5e80d6d12924308bfb2f935927778
John Doe <pirungu9@gmail.com>
2020-08-17 13:57:26 +0300

    GET envelope's recipients

--- routes/index.js
+++ routes/index.js
@@ -17,21 +17,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'the Docusign APIs' });
 });
 
-/* GET - Get a list of account's Envelopes */
-router.get('/templates', function (req, res, next) {
-  axios.get(`accounts/${accountId}/templates`)
+const docusignGet = (req, res, next, endpoint) => {
+  axios.get(endpoint)
   .then(response => {
-    console.log(response.data);
     res.status(response.status).send({
       data: response.data
     })
   })
   .catch(error => {
-    console.log(error.response);
     res.status(error.response.status).send({
       data: error.response.data
     });
   });
+}
+
+/* GET - Get a list of account's Envelopes */
+router.get('/templates', function (req, res, next) {
+  const endpoint = `accounts/${accountId}/templates`;
+  docusignGet(req, res, next, endpoint);
 });
 
 /* POST - Create a new Envelope */
@@ -53,19 +56,14 @@ router.post('/envelopes',  function (req, res, next) {
 
 /* GET - Get a list of Envelopes' documents */
 router.get('/envelopes/:envelopeId/documents', function(req, res, next) {
-  axios.get(`accounts/${accountId}/envelopes/${req.params.envelopeId}/documents`)
-  .then(response => {
-    console.log(response.data);
-    res.status(response.status).send({
-      data: response.data
-    })
-  })
-  .catch(error => {
-    console.log(error.response);
-    res.status(error.response.status).send({
-      data: error.response.data
-    });
-  });
+  const endpoint = `accounts/${accountId}/envelopes/${req.params.envelopeId}/documents`;
+  docusignGet(req, res, next, endpoint);
+});
+
+/* GET - Get a list of Envelopes' recipients */
+router.get('/envelopes/:envelopeId/recipients', function(req, res, next) {
+  const endpoint = `accounts/${accountId}/envelopes/${req.params.envelopeId}/recipients`;
+  docusignGet(req, res, next, endpoint);
 });
 
 module.exports = router;
